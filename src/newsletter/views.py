cf814ff1d41b3567ef5b47249382eb071fc180d2
achiengcindy <achiengcindy36@gmail.com>
2018-06-03 17:42:01 +0300

    validating email fiels

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -6,14 +6,16 @@ def newsletter_subscribe(request):
   if request.method == 'POST':
     form = NewsUserForm(request.POST)
     if form.is_valid():
-      instance = form.save()
-      print('your email is already added to our database')
+      instance = form.save(commit=False)
+      if NewsUsers.objects.filter(email=instance.email).exists():
+        print('your email Already exists in our database')
+      else:
+        instance.save()
+        print('your email has been submitted to our database')
   else:
     form = NewsUserForm()
   context = {'form':form}
   template = "newsletter/subscribe.html"
   return render(request, template, context)
-
-
 
 
