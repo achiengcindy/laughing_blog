8e0a840c966aef613c47b900f006f68e13dbcb77
achiengcindy <achiengcindy36@gmail.com>
2018-06-04 18:14:57 +0300

    sending  alternative emails

--- src/laughing_blog/settings.py
+++ src/laughing_blog/settings.py
@@ -8,8 +8,7 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 SECRET_KEY = config('SECRET_KEY')
 
--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -10,19 +10,17 @@ def newsletter_subscribe(request):
   if request.method == 'POST':
     form = NewsUserForm(request.POST)
     if form.is_valid():
-      instance = form.save(commit=False)#we dont want to save just yet
+      instance = form.save(commit=False) #we dont want to save just yet
       if NewsUsers.objects.filter(email=instance.email).exists():
         messages.warning(request, "your Email Already exists in our database")
       else:
         instance.save()
         messages.success(request, "your Email has been submitted to our database")
-        #send_mail('Laughing blog tutorial series','welcome','mail@achiengcindy.com',['instance.email'], fail_silently=False)
-        send_multiple_email('name', 'email')
+        send_multiple_email(instance.name, instance.email)
         
   else:
     form = NewsUserForm()
+
   context = {'form':form}
   template = "newsletter/subscribe.html"
   return render(request, template, context)
-
-
