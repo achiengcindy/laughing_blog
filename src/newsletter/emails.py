7bc8d3e7e2fb3df9893de2585151a5ee6dd9e8f2
achiengcindy <achiengcindy36@gmail.com>
2018-06-04 11:50:26 +0300

    creating emails.py

---
+++ src/newsletter/emails.py
@@ -0,0 +1,14 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+def send_multiple_email(name, receiver):
+    subject = 'Welcome to Laughing Blog Tutorial'
+    sender = 'mail@achiengcindy.com'
+
+    #passing in the context vairables
+    text_template = render_to_string('newsletter/subscribe.txt',{"name": name})
+    html_template = render_to_string('newsletter/email-subscribe.html',{"name": name})
+
+    message = EmailMultiAlternatives(subject,text_template,sender, [receiver])
+    message.attach_alternative(html_template, "text/html")
+    message.send()
